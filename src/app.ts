fdf0648cdf43175ae04b118bfe95b3a9e1aa0042
Rasel Ahmed <raselwebdev@hotmail.com>
2025-07-26 01:20:07 +0300

    Modified cors

--- src/app.ts
+++ src/app.ts
@@ -4,18 +4,29 @@ import { router } from "./app/routes";
 import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
 import notFound from "./app/middlewares/notFound";
 import cookieParser from "cookie-parser";
+import { envVars } from "./app/config/env";
 
 const app = express();
 
 app.use(cookieParser());
 app.use(express.json());
-
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+const allowedOrigins = envVars.FRONTEND_URL
+  ? envVars.FRONTEND_URL.split(",")
+  : [];
+const corsOption = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  origin: (origin: any, callback: any) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: "GET, POST, PUT, DELETE, PATCH",
+  optionsSuccessStatus: 200,
+  credentials: true,
+};
+app.use(cors(corsOption));
 
 app.use("/api/v1", router);
 
